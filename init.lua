b281f163a57f5814fe41cbd07d979f239b87a531
Scratch202 <102663198+Scratch202@users.noreply.github.com>
2023-05-06 13:14:00 +0900

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,28 @@
+print("[mod]    Character&youtuber load OK.")
+
+minetest.register_node("newblockmod:takobondo", {
+    description = "takobondo",
+    tiles = {"textures1.png"},
+    groups = {cracky = 1}
+})
+
+
+minetest.register_node("newblockmod:sumanaisensei", {
+    description = "sumanai",
+    tiles = {"textures2.png"},
+    groups = {cracky = 1}
+})
+
+
+minetest.register_node("newblockmod:enderdoragon", {
+    description = "ender block",
+    tiles = {"textures3.png"},
+    groups = {cracky = 1}
+})
+
+
+minetest.register_node("newblockmod:witherstorm", {
+    description = "wither storm",
+    tiles = {"textures4.png"},
+    groups = {cracky = 1}
+})
